5c60b45d0af5bd8f3a7151bfacfe43c56af09ecf
Pedro P. Candel <pedro@joyent.com>
2016-08-02 13:04:37 +0200

    TOOLS-1487 sdcadm improve function for handling circular references Reviewed by: Trent Mick <trent.mick@joyent.com> Reviewed by: Joshua M. Clulow <jmc@joyent.com>

---
+++ test/common.test.js
@@ -0,0 +1,31 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright 2016 Joyent, Inc.
+ */
+
+var test = require('tape').test;
+var common = require('/opt/smartdc/sdcadm/lib/common.js');
+
+test('safeCycles', function (t) {
+    var a = {
+        foo: {
+            bar: null
+        }
+    };
+    a.foo.bar = a;
+
+    t.throws(function () {
+        JSON.stringify(a);
+    }, TypeError);
+
+    t.doesNotThrow(function () {
+        JSON.stringify(a, common.safeCycles());
+    }, TypeError);
+
+    t.end();
+});
