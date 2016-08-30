ba692d78ab118dc928fe87416d441402c022f2d9
Pedro P. Candel <pedro@joyent.com>
2016-08-30 12:46:43 +0200

    TOOLS-1537 sdcadm create times out checking instance health Reviewed by: Trent Mick <trent.mick@joyent.com>

--- test/create.test.js
+++ test/create.test.js
@@ -5,16 +5,18 @@
  */
 
 /*
- * Copyright (c) 2015, Joyent, Inc.
+ * Copyright 2016, Joyent, Inc.
  */
 
 
 var test = require('tape').test;
 var exec = require('child_process').exec;
+var util = require('util');
 
 
 var HEADNODE_UUID = '';
-var NEW_NAPI_UUID = '';
+var NAPI_UUID = '';
+var NAPI_UUID_2 = '';
 var NUM_NAPI = 0;
 
 
@@ -26,22 +28,33 @@ function getNumNapi(cb) {
         }
 
         var lines = stdout.split('\n');
-        return cb(null, lines.length);
+        cb(null, lines.length);
     });
 }
 
 
+function getLatestImgAvail(cb) {
+    var cmd = 'updates-imgadm list name=napi --latest --json';
+    exec(cmd, function (err, stdout, stderr) {
+        if (err) {
+            return cb(err);
+        }
+
+        var latestImgUuid = JSON.parse(stdout.trim())[0].uuid;
+        cb(null, latestImgUuid);
+    });
+}
+
 test('setup', function (t) {
-    var cmd = 'sdc-cnapi /servers?alias=headnode | json -H';
+    var cmd = 'sysinfo | json UUID';
 
     exec(cmd, function (err, stdout, stderr) {
-        t.ifError(err);
-        t.equal(stderr, '');
-
-        HEADNODE_UUID = JSON.parse(stdout)[0].uuid;
+        t.ifError(err, 'CNAPI error');
+        t.equal(stderr, '', 'Empty stderr');
+        HEADNODE_UUID = stdout.trim();
 
         getNumNapi(function (err2, numNapi) {
-            t.ifError(err2);
+            t.ifError(err2, 'vmadm list error');
             t.ok(numNapi >= 1, 'at least one napi instance exists');
             NUM_NAPI = numNapi;
 
@@ -53,21 +66,21 @@ test('setup', function (t) {
 
 test('sdcadm create --help', function (t) {
     exec('sdcadm create --help', function (err, stdout, stderr) {
-        t.ifError(err);
+        t.ifError(err, 'Execution error');
 
         t.notEqual(stdout.indexOf('sdcadm create <svc>'), -1);
-        t.equal(stderr, '');
+        t.equal(stderr, '', 'Empty stderr');
 
         t.end();
     });
 });
 
 
+// Mandatory --server arg:
 test('sdcadm create napi', function (t) {
     exec('sdcadm create napi', function (err, stdout, stderr) {
-        t.ok(err);
+        t.ok(err, 'Execution error');
 
-        t.equal(stdout, '');
         t.notEqual(stderr.indexOf('Must specify server uuid'), -1);
 
         t.end();
@@ -75,13 +88,13 @@ test('sdcadm create napi', function (t) {
 });
 
 
+// Mandatory --skip-ha-check for non HA service:
 test('sdcadm create napi --dry-run --server', function (t) {
     var cmd = 'sdcadm create napi --dry-run --server=' + HEADNODE_UUID;
 
     exec(cmd, function (err, stdout, stderr) {
-        t.ok(err);
+        t.ok(err, 'Execution error');
 
-        t.equal(stdout, '');
         t.notEqual(stderr.indexOf('Must provide \'--skip-ha-check\''), -1);
 
         t.end();
@@ -89,15 +102,16 @@ test('sdcadm create napi --dry-run --server', function (t) {
 });
 
 
+// Test --dry-run:
 test('sdcadm create napi --dry-run --skip-ha-check -y --server', function (t) {
     var cmd = 'sdcadm create napi --dry-run --skip-ha-check --yes --server=' +
               HEADNODE_UUID;
 
     exec(cmd, function (err, stdout, stderr) {
-        t.ifError(err);
+        t.ifError(err, 'Execution error');
 
         t.notEqual(stdout.indexOf('Created successfully'), -1);
-        t.equal(stderr, '');
+        t.equal(stderr, '', 'Empty stderr');
 
         getNumNapi(function (err2, numNapi) {
             t.ifError(err2);
@@ -108,22 +122,23 @@ test('sdcadm create napi --dry-run --skip-ha-check -y --server', function (t) {
 });
 
 
+// Real create test:
 test('sdcadm create napi --skip-ha-check --yes --server', function (t) {
     var cmd = 'sdcadm create napi --skip-ha-check --yes --server=' +
               HEADNODE_UUID;
 
     exec(cmd, function (err, stdout, stderr) {
-        t.ifError(err);
-        t.equal(stderr, '');
+        t.ifError(err, 'Execution error');
+        t.equal(stderr, '', 'Empty stderr');
 
         t.notEqual(stdout.indexOf('Created successfully'), -1);
 
         getNumNapi(function (err2, numNapi) {
-            t.ifError(err2);
+            t.ifError(err2, 'vmadm list error');
 
             t.equal(numNapi, NUM_NAPI + 1);
             // JSSTYLED
-            NEW_NAPI_UUID = stdout.match(/Instance "(.+?)"/)[1];
+            NAPI_UUID = stdout.match(/Instance "(.+?)"/)[1];
 
             t.end();
         });
@@ -131,16 +146,45 @@ test('sdcadm create napi --skip-ha-check --yes --server', function (t) {
 });
 
 
-// TODO: --image
+// Create test with latest available image:
+test('sdcadm create napi --skip-ha-check -y -s --image', function (t) {
+    getLatestImgAvail(function (updatesErr, latestImageUuid) {
+        t.ifError(updatesErr, 'updates-imgadm list error');
 
+        var cmd = 'sdcadm create napi --skip-ha-check --yes --server=' +
+                  HEADNODE_UUID + ' --image=' + latestImageUuid;
+        exec(cmd, function (err, stdout, stderr) {
+            t.ifError(err, 'Execution error');
+            t.equal(stderr, '', 'Empty stderr');
+
+            t.notEqual(stdout.indexOf('Created successfully'), -1);
+
+            getNumNapi(function (err2, numNapi) {
+                t.ifError(err2, 'vmadm list error');
+
+                t.equal(numNapi, NUM_NAPI + 2);
+
+                // JSSTYLED
+                NAPI_UUID_2 = stdout.match(/Instance "(.+?)"/)[1];
+
+                t.end();
+            });
+        });
+    });
+});
 
 test('teardown', function (t) {
-    exec('vmadm destroy ' + NEW_NAPI_UUID, function (err, stdout, stderr) {
-        t.ifError(err);
+    var cmd = 'sdc-sapi /instances/%s -X DELETE';
 
-        t.equal(stdout, '');
-        t.notEqual(stderr.indexOf('Successfully deleted VM'), -1);
+    exec(util.format(cmd, NAPI_UUID), function (err, stdout, stderr) {
+        t.ifError(err, 'Execution error');
+        t.equal(stderr, '', 'Empty stderr');
 
-        t.end();
+        exec(util.format(cmd, NAPI_UUID_2), function (err2, stdout2, stderr2) {
+            t.ifError(err2, 'Execution error');
+            t.equal(stderr2, '', 'Empty stderr');
+
+            t.end();
+        });
     });
 });
